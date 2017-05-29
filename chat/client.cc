946b108dd25f75dc69a301f1e00ab4d9ec0da55d
Ubuntu <ubuntu@ip-172-31-4-40.ap-northeast-2.compute.internal>
2017-05-29 14:23:36 +0000

    클라이언트 소스에서 헤더파일과 소스파일로 분리한다.

--- chat/client.cc
+++ chat/client.cc
@@ -1,106 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+#include "client.hpp"
 
-#include <iostream>
-#include <thread>
+ChatClient::ChatClient(const char *serverIP, const char *serverPort, const char *clientName) {
+	sprintf(this->name, "[%s]", clientName);
+	this->sock = socket(PF_INET, SOCK_STREAM, 0);
 
-#define BUF_SIZE 100
-#define NAME_SIZE 20
+	memset(&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr(serverIP); // x.x.x.x(IPv4)로 되어있는 텍스트를 빅엑디언 네트워크 주소로 바꾼다.
+	serverAddress.sin_port = htons(atoi(serverPort));
+}
 
-using std::cout;
-using std::cin;
-using std::endl;
+ChatClient::~ChatClient() {
+	close(sock);
+}
 
-class ChatClient
-{
-private:
-	static char name[NAME_SIZE];
-	static char message[BUF_SIZE];
-
-	int sock;
-	struct sockaddr_in serverAddress;
-
-public:
-	ChatClient(const char *serverIP, const char *serverPort, const char *clientName)
-	{
-		sprintf(this->name, "[%s]", clientName);
-		this->sock = socket(PF_INET, SOCK_STREAM, 0);
-
-		memset(&serverAddress, 0, sizeof(serverAddress));
-		serverAddress.sin_family = AF_INET;
-		serverAddress.sin_addr.s_addr = inet_addr(serverIP); // x.x.x.x(IPv4)로 되어있는 텍스트를 빅엑디언 네트워크 주소로 바꾼다.
-		serverAddress.sin_port = htons(atoi(serverPort));
+void ChatClient::Start() {
+	if(connect(sock, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+		SendException("connect() 에러발생");
 	}
 
-	~ChatClient()
-	{
-		close(sock);
-	}
+	std::thread senderThread(SendMessage, (void*)&(this->sock));
+	std::thread receiverThread(ReceiveMessage, (void*)&(this->sock));
+	senderThread.join();
+	receiverThread.join();
+}
 
-	void Start()
-	{
-		if(connect(sock, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
-			SendException("connect() 에러발생");
+void* ChatClient::SendMessage(void *socket) {
+	int sock = *((int*)socket);
+	char nameMessage[NAME_SIZE + BUF_SIZE];
 
-		std::thread senderThread(SendMessage, (void*)&(this->sock));
-		std::thread receiverThread(ReceiveMessage, (void*)&(this->sock));
-		senderThread.join();
-		receiverThread.join();
-	}
-
-	static void *SendMessage(void *socket)
-	{
-		int sock = *((int*)socket);
-		char nameMessage[NAME_SIZE + BUF_SIZE];
-
-		while(1)
-		{
-			fgets(message, BUF_SIZE, stdin);
-			if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
-			{
-				close(sock);
-				return (void*)0;
-			}
-			sprintf(nameMessage, "%s %s", name, message);
-			send(sock, nameMessage, strlen(nameMessage) +1, 0);
+	while(1) {
+		fgets(message, BUF_SIZE, stdin);
+		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n")) {
+			close(sock);
+			return (void*)0;
 		}
-		return NULL;
+		sprintf(nameMessage, "%s %s", name, message);
+		send(sock, nameMessage, strlen(nameMessage) +1, 0);
 	}
+	return NULL;
+}
 
-	static void *ReceiveMessage(void *socket)
-	{
-		int sock = *((int*)socket);
-		char nameMessage[NAME_SIZE + BUF_SIZE];
+void* ChatClient::ReceiveMessage(void *socket) {
+	int sock = *((int*)socket);
+	char nameMessage[NAME_SIZE + BUF_SIZE];
 
-		while(1)
-		{
-			int stringLength = recv(sock, nameMessage, sizeof(nameMessage), 0);
-			if(stringLength == -1)
-			{
-				return (void*)-1;
-			}
-			nameMessage[stringLength] = 0;
-			fputs(nameMessage, stdout);
+	while(1) {
+		int stringLength = recv(sock, nameMessage, sizeof(nameMessage), 0);
+		if(stringLength == -1) {
+			return (void*)-1;
 		}
-		return NULL;
+		nameMessage[stringLength] = 0;
+		fputs(nameMessage, stdout);
 	}
+	return NULL;
+}
 
-	void SendException(const char *message)
-	{
-		cout<<message<<endl;
-	}
-};
+void ChatClient::SendException(const char *message) {
+	cout<<message<<endl;
+}
 
 char ChatClient::name[NAME_SIZE] = "[Anonymous]";
 char ChatClient::message[BUF_SIZE] = {0,};
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	cout<<"클라이언트 접속 시도"<<endl;
 	ChatClient *client = new ChatClient("52.78.193.49", "9000", "Tester");
 	client->Start();
---
+++ chat/client.hpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <iostream>
+#include <thread>
+
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class ChatClient {
+private:
+	static char name[NAME_SIZE];
+	static char message[BUF_SIZE];
+
+	int sock;
+	struct sockaddr_in serverAddress;
+
+public:
+	ChatClient(const char *serverIP, const char *serverPort, const char *clientName);
+	~ChatClient();
+	void Start();
+	static void *SendMessage(void *socket);
+	static void *ReceiveMessage(void *socket);
+	void SendException(const char *message);
+};
