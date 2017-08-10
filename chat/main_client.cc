c57aa02388664bab3b4be16b71bc514b4ee14b94
Ubuntu <ubuntu@ip-172-31-4-40.ap-northeast-2.compute.internal>
2017-08-10 11:14:43 +0000

    클라이언트 접속시 아이디 입력하기 로직을 추가한다.

--- chat/main_client.cc
+++ chat/main_client.cc
@@ -1,8 +1,14 @@
 #include "client.hpp"
 
+#define MAX_ID_SIZE 100
+
 int main(int argc, char *argv[]) {
+	char clientID[MAX_ID_SIZE];
 	cout<<"클라이언트 접속 시도"<<endl;
-	ChatClient *client = new ChatClient("52.78.193.49", "9000", "Tester");
+	cout<<"채팅에 사용할 아이디를 입력하세요"<<endl;
+	cin>> clientID;
+	
+	ChatClient *client = new ChatClient("52.78.193.49", "9000", clientID);
 	client->Start();
 	return 0;
 }
