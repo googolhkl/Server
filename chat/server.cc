7ab03d819e41ab2d2ba5b486b3f11092c258053f
Ubuntu <ubuntu@ip-172-31-4-40.ap-northeast-2.compute.internal>
2017-08-28 12:09:18 +0000

    다중채팅 기초를 잡는다.(쓰레드 필요)

--- chat/server.cc
+++ chat/server.cc
@@ -8,7 +8,13 @@
 
 #define BUF_SIZE 1024
 #define EPOLL_SIZE 50
+
+#define MAX_CLIENT 255
+
 void error_handling(char *message);
+
+int client_count = 0;
+int client_sockets[MAX_CLIENT];
 
 int main(int argc, char* argv[])
 {
@@ -79,20 +85,42 @@ int main(int argc, char* argv[])
 				event.events = EPOLLIN;
 				event.data.fd = clnt_sock;
 				epoll_ctl(epfd, EPOLL_CTL_ADD, clnt_sock, &event);
-				printf("연결된 클라이언트: %d\n", clnt_sock);
+				client_sockets[client_count++] = clnt_sock;  // 배열에 클라이언트 정보 삽입
+				for(int i=0; i<client_count; i++)
+				{
+					printf("array[%d]: %d\n", i,client_sockets[i]);
+				}
 			}
 			else
 			{
 				str_len = read(ep_events[i].data.fd, buf, BUF_SIZE);
 				if(str_len == 0) // 종료 요청
 				{
+					// 배열에 있는 클라이언트 정보 삭제
+					for(int j=0; j<client_count; j++)
+					{
+						if(ep_events[i].data.fd == client_sockets[j])
+						{
+							while(j++ <= client_count-1)
+							{
+								client_sockets[j] = client_sockets[j+1];
+								client_count--;
+							}
+							break;
+						}
+					}
+
 					epoll_ctl(epfd, EPOLL_CTL_DEL, ep_events[i].data.fd, NULL);
 					close(ep_events[i].data.fd);
 					printf("종료된 클라이언트: %d\n", ep_events[i].data.fd);
 				}
 				else
 				{
-					write(ep_events[i].data.fd, buf, str_len); // 에코
+					for(int i=0; i<client_count; i++)
+					{
+						write(client_sockets[i], buf, str_len); // 에코
+					}
+					//write(ep_events[i].data.fd, buf, str_len); // 에코
 				}
 			}
 		}
